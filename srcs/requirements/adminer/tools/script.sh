71f3800502ae26efa71b57d3fddddd8771fd4848
driely01 <elyaagoubi.works@gmail.com>
2024-02-03 08:25:14 -0500

    fix adminer with nginx

--- srcs/requirements/adminer/tools/script.sh
+++ srcs/requirements/adminer/tools/script.sh
@@ -1,4 +1,5 @@
 #!bin/sh
-mkdir -p /var/www/html/adminer
-wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer/index.php && chmod +x /var/www/html/adminer/index.php
+mkdir -p /var/www/html/adminer && \
+wget "http://www.adminer.org/latest.php" -O /var/www/html/adminer/index.php && \
+chmod +x /var/www/html/adminer/index.php
 exec "$@"
